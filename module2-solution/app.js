f6f05ca6d4c02b24f4606a0297efd43050ae41de
admin <designjm88@gmail.com>
2017-11-17 17:00:24 +0100

    assignment-2: Create app according to assigment2 description

---
+++ module2-solution/app.js
@@ -0,0 +1,60 @@
+(function() {
+  "use strict";
+
+  angular.module("ShoppingListCheckOff", [])
+  .controller("ToBuyController", ToBuyController)
+  .controller("AlreadyBoughtController", AlreadyBoughtController)
+  .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var controller = this;
+
+    controller.getItemsToBuy = ShoppingListCheckOffService.getItemsToBuy();
+
+    controller.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buy(itemIndex);
+      ShoppingListCheckOffService.removeFromBuyItems(itemIndex);
+    };
+  }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var controller = this;
+
+    controller.getBoughtItems = ShoppingListCheckOffService.getBoughtItems();
+
+  }
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  var itemsToBuy = [{ name: "cookies", quantity: 10 }, { name: "pies", quantity: 9 },
+                    { name: "tomatoes", quantity: 8 }, {name: "chocolates", quantity: 7 },
+                    { name: "apples", quantity: 6 }];
+  var boughtItems = [];
+
+  service.getItemsToBuy = function () {
+    return itemsToBuy;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+
+  service.buy = function (itemIndex) {
+    var item = itemsToBuy[itemIndex];
+    var item = {
+      name:  item.name,
+      quantity: item.quantity
+    };
+    boughtItems.push(item);
+  };
+
+  service.removeFromBuyItems = function (itemIndex) {
+    itemsToBuy.splice(itemIndex, 1);
+  };
+
+}
+
+})();
